c574b5bab7b5769f17bbf1e1d627f9dac578ae04
Kevin Lee <pwntastickevin@gmail.com>
2017-10-30 22:35:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $(".crypto").click(function(){
+    $(".crypto").css("fill", "red")
+    $(".svg").css("height", "20rem")
+    $(".svg").css("width", "20rem")
+    $(".creek").append('.body1')
+  });
+
+
+})
